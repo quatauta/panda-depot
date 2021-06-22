2b35f0a26ce403c48b15b197f9c49f96e24638a9
Daniel Schömer <daniel.schoemer@gmx.net>
2021-06-22 11:03:35 +0000

    Resolve "Iteration D2 - Connecting Products to Carts"

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :error_if_referenced_by_line_item
+
   validates :image_url, allow_blank: true, format: {
     with: %r{\A.*\.(gif|jpg|png|svg)\z}i,
     message: "must be a URL for a GIF, JPG, PNG, or SVG image."
@@ -7,4 +11,14 @@ class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :title, length: {minimum: 10}
+
+  private
+
+  # Raise an error if this product is referenced by a line item
+  def error_if_referenced_by_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw :abort
+    end
+  end
 end

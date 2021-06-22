3861ff569ea24f88b45d2909a35ffbf3d62b48dc
Daniel Schoemer <daniel.schoemer@gmx.net>
2021-06-22 15:20:59 +0200

    Added store product list view counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
+    session[:counter] = (session[:counter] ||= 0) + 1
+    @counter = session[:counter]
     @products = Product.order(:title)
   end
 end

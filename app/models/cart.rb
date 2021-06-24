778fb0ec1b3de124be130ccebaea6753290c5140
Daniel Schömer <daniel.schoemer@gmx.net>
2021-06-24 16:16:11 +0000

    Resolve "Iteration E2 - Handling Errors"

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,6 @@ class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
   def add_product(product)
-    puts product
     current_item = line_items.find_by(product_id: product.id)
 
     if current_item
